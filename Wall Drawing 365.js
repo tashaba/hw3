02bb3f4d80dd890ea1efef0af25e6fac3cc7b1ad
tashaba <35852644+tashaba@users.noreply.github.com>
2018-02-09 19:35:22 -0800

    Update and rename WALL DRAWING 365.js to Wall Drawing 365.js

--- Wall Drawing 365.js
+++ Wall Drawing 365.js
@@ -1,25 +1,26 @@
 
 
-var c = color(240, 240,240);
-fill(c); 
-strokeWeight(4); 
+
+var c = color(201, 204, 207);
+fill (c);
+strokeWeight(0); 
 stroke(51);
 rect(2,2, 170, 170); 
 
 var c = color(129, 120,128);
 fill(c); 
-strokeWeight(4); 
+strokeWeight(0); 
 stroke(51);
 rect(168,2, 170, 170); 
 
 var c = color(128, 128, 140);
 fill(c); 
-strokeWeight(4); 
+strokeWeight(0); 
 stroke(51);
-rect(2,168, 165, 168); 
+rect(2,168, 166, 168); 
 
-var c = color(128, 128, 128);
+var c = color(54, 48, 54);
 fill(c); 
-strokeWeight(4); 
+strokeWeight(0); 
 stroke(51);
 rect(168,168, 170, 168); 

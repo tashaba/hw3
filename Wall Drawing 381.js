5078c5a8f865f2bb9c6f338f7ed26eded947ae32
tashaba <35852644+tashaba@users.noreply.github.com>
2018-02-09 19:32:43 -0800

    Create Wall Drawing 381.js

---
+++ Wall Drawing 381.js
@@ -0,0 +1,28 @@
+
+
+
+
+
+var c = color(201, 204, 207);
+fill (c);
+strokeWeight(0); 
+stroke(51);
+rect(2,2, 170, 170); 
+
+var c = color(255, 255, 153);
+fill(c); 
+strokeWeight(0); 
+stroke(51);
+rect(168,2, 170, 170); 
+
+var c = color(255, 102, 102);
+fill(c); 
+strokeWeight(0); 
+stroke(51);
+rect(2,168, 166, 168); 
+
+var c = color(0, 191, 255);
+fill(c); 
+strokeWeight(0); 
+stroke(51);
+rect(168,168, 170, 168); 

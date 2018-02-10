dd8dfd51a986f8316e208fbb605e74ef3fef0a3c
tashaba <35852644+tashaba@users.noreply.github.com>
2018-02-09 19:24:34 -0800

    Create WALL DRAWING 365.js

---
+++ WALL DRAWING 365.js
@@ -0,0 +1,25 @@
+
+
+var c = color(240, 240,240);
+fill(c); 
+strokeWeight(4); 
+stroke(51);
+rect(2,2, 170, 170); 
+
+var c = color(129, 120,128);
+fill(c); 
+strokeWeight(4); 
+stroke(51);
+rect(168,2, 170, 170); 
+
+var c = color(128, 128, 140);
+fill(c); 
+strokeWeight(4); 
+stroke(51);
+rect(2,168, 165, 168); 
+
+var c = color(128, 128, 128);
+fill(c); 
+strokeWeight(4); 
+stroke(51);
+rect(168,168, 170, 168); 
